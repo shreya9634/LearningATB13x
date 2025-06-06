e438aab61183f2cf933f187a60af36f4b3bfac18
Shreya <shreyaverma247@gmail.com>
2025-06-06 16:31:49 +0530

    new adn Lab002 added

--- ex_01_Java_Basic/Lab0001_HelloWorld.java
+++ ex_01_Java_Basic/Lab0001_HelloWorld.java
@@ -2,6 +2,6 @@ package ex_01_Java_Basic;
 
 public class Lab0001_HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world its shreya Verma");
+        System.out.println("Hello world its Shreya Verma");
     }
 }
