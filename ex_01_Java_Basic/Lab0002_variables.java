bf97ce73a256d0ce02b918c192d3690d997b77a4
Shreya <shreyaverma247@gmail.com>
2025-06-12 15:46:00 +0530

    printf() for 9 table

---
+++ ex_01_Java_Basic/Lab0002_variables.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab0002_variables {
+    public static void main(String[] args) {
+        int a = 10;
+        a = 65 + 1;
+        System.out.println(a);
+    }
+
+
+    }
---
+++ ex_01_Java_Basic/Lab0003_Printf.java
@@ -0,0 +1,27 @@
+package ex_01_Java_Basic;
+
+public class Lab0003_Printf {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+        int f = 6;
+        int g = 7;
+        int h = 8;
+        int i = 9;
+        int j = 10;
+        System.out.printf("%d x %d = %d",9,1,9).println();
+        System.out.printf("%d x %d = %d",9,2,18).println();
+        System.out.printf("%d x %d = %d",9,3,27).println();
+        System.out.printf("%d x %d = %d",9,4,36).println();
+        System.out.printf("%d x %d = %d",9,5,45).println();
+        System.out.printf("%d x %d = %d",9,6,54).println();
+        System.out.printf("%d x %d = %d",9,7,63).println();
+        System.out.printf("%d x %d = %d",9,8,72).println();
+        System.out.printf("%d x %d = %d",9,9,81).println();
+        System.out.printf("%d x %d = %d",9,10,90).println();
+
+    }
+}
