104945b8307d67360fde50bdb3abf424bf5541b9
Shreya <shreyaverma247@gmail.com>
2025-06-19 07:56:59 +0530

    test

---
+++ ex_01_Java_Basic/test2_operator.java
@@ -0,0 +1,20 @@
+package ex_01_Java_Basic;
+
+public class test2_operator {
+    public static void main(String[] args) {
+        System.out.println("arithematic operator");
+        int i = 10;
+        int j = 2;
+
+        int sum = i+j;
+        System.out.println("addition(i+j):"+sum);
+
+        int remainder = i % j;
+        System.out.println("modulus(i%j):"+remainder);
+
+        int multiplication = i*j;
+        System.out.println("multiplication(i*j):"+multiplication);
+
+
+    }
+}
