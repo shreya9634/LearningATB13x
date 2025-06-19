f6b95c78d33ebb41122441c400c6a3bd2c4a7db3
Shreya <shreyaverma247@gmail.com>
2025-06-19 07:43:30 +0530

    test

---
+++ ex_01_Java_Basic/test.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class test {
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println("integer = 5, int = data type");
+    }
+}
